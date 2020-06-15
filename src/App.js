c775873641ee1f5628440219d677e5543f280950
ErickZarat <ezarat15@gmail.com>
2020-06-15 16:24:25 -0600

    add phone input wrapper

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import 'react-phone-number-input/style.css'
+import PhoneInput, { isValidPhoneNumber } from 'react-phone-number-input'
+
+
 
 function App() {
+  const [value, setValue] = useState()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PhoneInput
+      placeholder="Numero de telefono"
+      defaultCountry="GT"
+      value={value}
+      onChange={setValue}/>
   );
 }
 
